c01edd2cd8e6ee69f246800dd6109cf6b980a6e6
Boye Borg <boye.borg@icloud.com>
2018-03-25 21:47:40 +0200

    add edit center of mass collector

---
+++ src/no/ntnu/boyebn/master/EditCenterOfMassCollector.java
@@ -0,0 +1,49 @@
+package no.ntnu.boyebn.master;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.eclipse.emf.ecore.EObject;
+
+import no.hal.learning.exercise.jdt.impl.JdtSourceEditEventImpl;
+
+public class EditCenterOfMassCollector implements DataCollector {
+	
+	Map<Long, Integer> edits;
+	
+	public EditCenterOfMassCollector() {
+		reset();
+	}
+
+	@Override
+	public void addEvent(EObject eObject) {
+		if (eObject instanceof JdtSourceEditEventImpl) {
+			JdtSourceEditEventImpl editEvent = (JdtSourceEditEventImpl) eObject;
+			edits.put(editEvent.getTimestamp(), editEvent.getSizeMeasure());
+		}
+	}
+
+	@Override
+	public String getName() {
+		return "edit_center_of_mass";
+	}
+
+	@Override
+	public String getResult() {
+		
+		final double res[] = {0, 0};
+		
+		edits.forEach((k, v) -> {
+			res[0] += k*v;
+			res[1] += v;
+		});
+		
+		return Long.toString(Math.round(res[0]/res[1]));
+	}
+
+	@Override
+	public void reset() {
+		edits = new TreeMap<>();
+	}
+
+}
