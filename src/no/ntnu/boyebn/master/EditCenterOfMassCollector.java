058e572198937e46416149b0e9bed826066c5542
Boye Borg <boye.borg@icloud.com>
2018-04-05 15:13:32 +0200

    add normalization and remove useless time before and after exercise

--- src/no/ntnu/boyebn/master/EditCenterOfMassCollector.java
+++ src/no/ntnu/boyebn/master/EditCenterOfMassCollector.java
@@ -1,6 +1,7 @@
 package no.ntnu.boyebn.master;
 
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.TreeMap;
 import java.util.concurrent.TimeUnit;
 
@@ -11,9 +12,11 @@ import no.hal.learning.exercise.jdt.impl.JdtSourceEditEventImpl;
 public class EditCenterOfMassCollector implements DataCollector {
 	
 	Map<Long, Integer> edits;
+	double result;
 	
 	static long firstEdit = Long.MAX_VALUE;
 	static long lastEdit = Long.MIN_VALUE;
+	static Map<Long, Integer> globalEdits = new TreeMap<>();
 	
 	public EditCenterOfMassCollector() {
 		edits = new TreeMap<>();
@@ -28,6 +31,7 @@ public class EditCenterOfMassCollector implements DataCollector {
 		if (eObject instanceof JdtSourceEditEventImpl) {
 			JdtSourceEditEventImpl editEvent = (JdtSourceEditEventImpl) eObject;
 			edits.put(editEvent.getTimestamp(), editEvent.getSizeMeasure());
+			globalEdits.put(editEvent.getTimestamp(), editEvent.getSizeMeasure());
 			
 			firstEdit = Math.min(editEvent.getTimestamp(), firstEdit);
 			lastEdit = Math.max(editEvent.getTimestamp(), lastEdit);
@@ -38,28 +42,74 @@ public class EditCenterOfMassCollector implements DataCollector {
 	public String getName() {
 		return "edit_center_of_mass";
 	}
+	
+	@Override
+	public void calculateResult() {
+		
+		int totalEditSize = globalEdits.values().stream().reduce(Integer::sum).get();
+		double percent = 0.001;
+		
+		int firstDay = -1;
+		int lastDay = -1;
+		
+		int totalDays = getDay(lastEdit);
+		
+		double[] globalDistribution = new double[totalDays+1];
+		
+		// initialize distribution
+		for (int i = 0; i < totalDays+1; i++) {
+			globalDistribution[i] = 0;
+		}
+		
+		// create distribution
+		for (Entry<Long, Integer> entry : globalEdits.entrySet()) {
+			globalDistribution[getDay(entry.getKey())] += entry.getValue();
+		}
+		
+		// find first day
+		for (int i = 0; i < totalDays+1; i++) {
+			if (globalDistribution[i] >= totalEditSize*percent) {
+				firstDay = i;
+				break;
+			}
+		}
+		
+		// find last day
+		for (int i = totalDays; i >= 0; i--) {
+			if (globalDistribution[i] >= totalEditSize*percent) {
+				lastDay = i;
+				break;
+			}
+		}
+		
+		Map<Long, Integer> newEdits = new TreeMap<>();
+		
+		for (Entry<Long, Integer> entry : edits.entrySet()) {
+			int day = getDay(entry.getKey());
+			if (day >= firstDay && day <= lastDay) {
+				newEdits.put(entry.getKey(), entry.getValue());
+			}
+		}
+		
+		int newTotalDays = lastDay - firstDay + 1;
+
+		final double[] val = {0.0, 0.0};
+		
+		for (Entry<Long, Integer> entry : newEdits.entrySet()) {
+			val[0] += Math.abs(entry.getValue());
+			val[1] += (getDay(entry.getKey())-firstDay)*entry.getValue();
+		}
+
+		if (val[0] > 0 && newTotalDays > 0) {
+			result = val[1]/(val[0]*newTotalDays);
+		} else {
+			result = 1.0;
+		}
+	}
 
 	@Override
 	public String getResult() {
-		
-		int totalDays = getDay(lastEdit);
-		final double[] val = {0.0, 0.0};
-		
-		edits.forEach((timestamp, size) -> {
-			val[0] += Math.abs(size);
-			val[1] += getDay(timestamp)*size;
-		});
-		
-		try {
-			Double result = val[1]/(val[0]*totalDays);
-			if (result.equals(Double.NaN)) {
-				return "1.0";
-			}
-			return Double.toString(result);
-		} catch (ArithmeticException e) {
-			return "1.0";
-		}
-		
+		return String.format("%.3f", result);
 	}
 
 }
