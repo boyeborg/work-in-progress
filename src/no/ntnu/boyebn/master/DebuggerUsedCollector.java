154a2afe7cc9c15f1fd221a69569597cdc6d412b
Boye Borg <boye.borg@icloud.com>
2018-04-05 09:58:43 +0200

    create collector testing if the debugger is used

---
+++ src/no/ntnu/boyebn/master/DebuggerUsedCollector.java
@@ -0,0 +1,34 @@
+package no.ntnu.boyebn.master;
+
+import org.eclipse.emf.ecore.EObject;
+
+import no.hal.learning.exercise.jdt.impl.JdtLaunchEventImpl;
+
+public class DebuggerUsedCollector implements DataCollector {
+	
+	int debuggerUsed;
+	
+	public DebuggerUsedCollector() {
+		debuggerUsed = 0;
+	}
+
+	@Override
+	public void addEvent(EObject eObject) {
+		if (eObject instanceof JdtLaunchEventImpl) {
+			if (((JdtLaunchEventImpl) eObject).getMode().equals("debug")) {
+				debuggerUsed = 1;
+			}
+		}
+	}
+
+	@Override
+	public String getName() {
+		return "debugger_used";
+	}
+
+	@Override
+	public String getResult() {
+		return Integer.toString(debuggerUsed);
+	}
+
+}
